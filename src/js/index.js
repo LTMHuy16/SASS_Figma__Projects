4439e6573061fdb6f6a06b62dc63c40b8e4d1bc7
Huy Le <huy@qdn.digital>
2022-09-08 08:58:37 +0700

    update header mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,13 @@
 /* TOGGLE MOBILE MENU */
 
-const toggleMobileMenu = () => {
-    const overlay = $('#overlay');
-    const menu = $("#header-menu-js");
-    const menuCloses = $("#close-menu-js, #overlay");
-    const btnBar = $("#button-bar-js");
-
-    btnBar.click(() => {
-        menu.addClass("show");
-        overlay.addClass('show')
-    });
-
-    menuCloses.click(function () {
-        if (menu.hasClass('show')) {
-            menu.removeClass("show");
-            overlay.removeClass('show')
-        }
-
-        return;
-    });
+const navbarEvents = () => {
+    $('.js-open-menu, .js-close-menu').click(() => {
+        $('.js-header-menu').toggleClass('is-open');
+    })
 };
 
 /* RUN ALL FUNCTIONS */
 
 $(document).ready(function () {
-    toggleMobileMenu();
+    navbarEvents();
 });
